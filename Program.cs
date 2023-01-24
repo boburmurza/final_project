ecd7a3611dc5de5db03d509fc4ba7da7160a7f6c
Bobur <boryamur56@gmail.com>
2023-01-24 20:15:44 +0300

    создали метод для заполнения массива

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//
+// создаем метод для заполнения массива с клавиатуры 
+
+string[] CreateArray(int n )
+{
+    string[] array = new string [ n];
+    for(int i = 0; i < n; i++){
+        
+            array[i] = Console.ReadLine();
+        
+    }
+    return array;
+}
+
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++){
+        
+            Console.Write($"{array[i]} ");
+       }
+    Console.WriteLine();
+}
+
+
+
+Console.Write("Please enter the number of columns in your array: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+string[] array= CreateArray(n);
+
+PrintArray(array);
+
+
+
+
