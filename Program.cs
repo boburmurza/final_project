7ace5bd06282035f5155171f744d195c01c58c7a
Bobur <boryamur56@gmail.com>
2023-01-26 20:38:07 +0300

    решил задачу

--- Program.cs
+++ Program.cs
@@ -1,37 +1,48 @@
 ﻿//
 // создаем метод для заполнения массива с клавиатуры 
 
-string[] CreateArray(int n )
+string[] CreateArray(int n)
 {
-    string[] array = new string [ n];
-    for(int i = 0; i < n; i++){
-        
-            array[i] = Console.ReadLine();
-        
+    string[] array = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Please enter  array element  : ");
+        array[i] = Console.ReadLine();
     }
     return array;
 }
 
 
-void PrintArray(string[] array)
+int SortingArray(string[] array)
 {
-    for(int i = 0; i < array.GetLength(0); i++){
-        
-            Console.Write($"{array[i]} ");
-       }
-    Console.WriteLine();
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    
+    return count;
 }
-
-
-
-Console.Write("Please enter the number of columns in your array: ");
+string[] NewArray(int count, string[] array)
+{
+    string[] arr = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arr[j] = array[i];
+            j++;
+        }
+    }
+    return arr;
+}
+Console.Write("введите  массива ");
 int n = Convert.ToInt32(Console.ReadLine());
-
-
-string[] array= CreateArray(n);
-
-PrintArray(array);
-
-
-
-
+string[] array = CreateArray(n);
+int number = SortingArray(array);
+string[] newArr = NewArray(number, array);
+for (int i = 0; i < newArr.Length; i++)
+    Console.Write(newArr[i] + " ");
